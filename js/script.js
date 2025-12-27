65b849ca5bb0929f005e64cb6a2c2cea92859b1b
Areth Kellejian <arethk@gmail.com>
2025-12-26 19:09:51 -0800

    got the layout working

--- js/script.js
+++ js/script.js
@@ -2,36 +2,70 @@ const GAME_ROWS = 10;
 const GAME_COLUMNS = 10;
 window.onload = function () {
     const container = document.querySelector(".container");
-    //const totalDivs = (GAME_ROWS + 2) * GAME_COLUMNS;
-
-    /*
-    document.getElementById("myDIV").style.gridTemplateRows = "200px";
-
-    TODO: rows:
-    11fr repeat(10, 1fr) 11fr
-
-    BIG subtract 2 BIG
-    */
-   container.style.gridTemplateRows = "repeat(" + (GAME_ROWS + 2) + ", 1fr)";
-   container.style.gridTemplateColumns = "repeat(" + GAME_COLUMNS + ", 1fr)";
+    // set css grid rows and columns
+    container.style.gridTemplateRows = "minmax(50px, 2fr) repeat(" + GAME_ROWS + ", minmax(10px, 1fr)) minmax(150px, 4fr)";
+    container.style.gridTemplateColumns = "repeat(" + GAME_COLUMNS + ", minmax(30px, 1fr))";
 
     // create header area
+    const headerGridName = "header";
+    const headerDiv = document.createElement("div");
+    headerDiv.classList.add("header");
+    headerDiv.style.gridArea = headerGridName;
+    container.appendChild(headerDiv);
+
+    // create header grid area
+    const headerGridNameList = [];
     for (let i = 0; i < GAME_COLUMNS; i++) {
-        const div = document.createElement("div");
-        div.classList.add("header");
-        container.appendChild(div);
+        headerGridNameList.push(headerGridName);
     }
+    container.style.gridTemplateAreas = '"' + headerGridNameList.join(" ") + '"';
 
     // create game area
-    for (let i = 0; i < GAME_ROWS * GAME_COLUMNS; i++) {
-        const div = document.createElement("div");
-        container.appendChild(div);
+    for (let i = 0; i < GAME_ROWS; i++) {
+        const gameGridNameList = [];
+        for (let j = 0; j < GAME_ROWS; j++) {
+            const cellGridName = "cell" + i + "" + j;
+            gameGridNameList.push(cellGridName);
+            const div = document.createElement("div");
+            div.style.gridArea = cellGridName;
+            container.appendChild(div);
+        }
+        container.style.gridTemplateAreas += '"' + gameGridNameList.join(" ") + '"';
     }
 
     // create footer area
+    const footerGridName = "footer";
+    const footerDiv = document.createElement("div");
+    footerDiv.classList.add("footer");
+    footerDiv.style.gridArea = footerGridName;
+
+    // create control buttons
+    const upButton = document.createElement("button");
+    upButton.id = "up";
+    upButton.innerText = "^";
+    const leftButton = document.createElement("button");
+    leftButton.id = "left";
+    leftButton.innerText = "<";
+    const rightButton = document.createElement("button");
+    rightButton.id = "right";
+    rightButton.innerText = ">";
+    const downButton = document.createElement("button");
+    downButton.id = "down";
+    downButton.classList.add("flip");
+    downButton.innerText = "^";
+    const leftRightDiv = document.createElement("div");
+    leftRightDiv.classList.add("left-right-container");
+    leftRightDiv.appendChild(leftButton);
+    leftRightDiv.appendChild(rightButton);
+    footerDiv.appendChild(upButton);
+    footerDiv.appendChild(leftRightDiv);
+    footerDiv.appendChild(downButton);
+    container.appendChild(footerDiv);
+
+    // create header grid area
+    const footerGridNameList = [];
     for (let i = 0; i < GAME_COLUMNS; i++) {
-        const div = document.createElement("div");
-        div.classList.add("footer");
-        container.appendChild(div);
+        footerGridNameList.push(footerGridName);
     }
+    container.style.gridTemplateAreas += '"' + footerGridNameList.join(" ") + '"';
 }
